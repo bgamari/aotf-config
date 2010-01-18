9ca94426c5503e9a835c94e1d4d4f261ca0847d1
Ben Gamari <bgamari@blinky.(none)>
2010-01-18 15:19:29 -0500

    Take channel numbers on command line

--- config_aotf.py
+++ config_aotf.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
+import sys
 from gooch_housego import FreqSynth
 
 f = FreqSynth('/dev/ttyUSB0')
 
-for ch in [4,7]:
+channels = map(int, sys.argv[1:])
+for ch in channels:
         f.select_channel(ch)
         f.set_mode('on')
         print 'Channel %d: ' % ch, f.get_status()
