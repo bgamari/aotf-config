5419f4ee9c042e443ef1727b3c4b92ae7527b2c3
Ben Gamari <bgamari.foss@gmail.com>
2012-01-12 13:10:39 -0500

    Initial commit of aotf-power-scan

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='aotf-config',
       author_email="bgamari@physics.umass.edu",
       url="http://goldnerlab.physics.umass.edu/wiki",
       py_modules=['gooch_housego', 'usbtmc'],
-      scripts=['aotf-ui', 'aotf-config', 'aotf-freq-scan'],
+      scripts=['aotf-ui', 'aotf-config', 'aotf-freq-scan', 'aotf-power-scan'],
       data_files=[
               ('share/aotf-config', ['aotf-channel.glade', 'aotf-ui.glade']),
               ('/etc/udev/rules.d', ['thorlabs-pm100.rules']),
