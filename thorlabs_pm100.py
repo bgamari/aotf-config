a6ff3fd0f02b3e5373c59de9b674c5b3f0c9ba0e
Ben Gamari <bgamari.foss@gmail.com>
2014-11-07 16:28:15 -0500

    Factor out Thorlabs PM100 logic

---
+++ thorlabs_pm100.py
@@ -0,0 +1,21 @@
+import usbtmc
+
+class ThorlabsPM100(object):
+    def __init__(self, device):
+        self.device = usbtmc.usbtmc(device)
+        self.device.write('CONF:POW')
+        
+    @staticmethod
+    def find():
+        for dev,name in usbtmc.probe().items():
+                if name.startswith('Thorlabs,PM100'):
+                        return ThorlabsPM100(dev)
+        return None
+
+    def set_wavelength(self, min, max):
+        self.device.write('SENSE:CORR:WAVELENGTH MIN %f' % min)
+        self.device.write('SENSE:CORR:WAVELENGTH MAX %f' % max)
+        
+    def read(self):
+        self.device.write('READ?')
+        return float(self.device.read())
